b12dfb53adfed6428398fffe163bd24a89faedac
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-30 13:33:34 +0530

    create navbar folder and router

--- my-app/src/home/index.jsx
+++ my-app/src/home/index.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import "./styles.css";
 
-const index = () => {
-    return (
-        <div class='container'>
-            <img
-                class='image'
-                src='https://cdn0.iconfinder.com/data/icons/online-education-butterscotch-vol-2/512/Questions_And_Answers-512.png'
-                alt=''></img>
-            <button class='start-quiz'>
-                <a class='start' href='/quiz-finsight-page/index.html'>
-                    Start Quiz
-                </a>
-            </button>
-        </div>
-    );
+const Home = () => {
+    return <div className='container'></div>;
 };
 
-export default index;
+export default Home;
