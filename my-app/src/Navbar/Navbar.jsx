253423738311150acce19f11f29e71b36b48113e
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-30 18:28:03 +0530

    fix: navbar

--- my-app/src/Navbar/Navbar.jsx
+++ my-app/src/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { AccountCircle, Search } from "@material-ui/icons";
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <div className='container'>
+            <div className='navContainer'>
                 <div className='left'>
                     <img
                         src='https://cdn-icons-png.flaticon.com/512/2784/2784530.png'
